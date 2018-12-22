409c8a257ec9e7511039fe0403984539254d62c4
Terasute <terasute@example.com>
2018-12-22 21:09:34 +0900

    コンフィグ変更

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ var config = {
 
   // Mode 'GITHUB' for Github Pages, 'SERVER' for Self Hosted
   // Defaults to Server mode if not specified
-  mode: 'SERVER',
+  mode: 'GITHUB',
 
   // If Github mode is set, your Github username, repo name, 
   // and branch to get files from.
